7f5199da2d247e2756bed3c01d4f812d815ae05e
l3l4z3 <83835005+l3l4z3@users.noreply.github.com>
2022-12-18 18:46:11 +0630

    Update 4-Assignment.c

--- 4-Assignment.c
+++ 4-Assignment.c
@@ -1,87 +1,44 @@
 #include "stdio.h"
+#include "iostream"
 #include "stdlib.h"
-//Functions
-void printWords();
-void findWords();
-void findWord2();
-//Global Variable
-int toFind1[10] = {119, 105, 110, 104, 116, 117, 116, 0, 0, 0};
-//Main Function
+#include "string.h"
+using namespace std;
+int count = 0;
 int main(){
 
-    // printWords();
-    findWords();
-    findWord2();
+    char name[7] = {'w','i','n','h','t','u','t'};
 
+//    printf("Enter a word to search :");
+//    scanf("%s", name);
+//    printf("The word you want to search : %s", name);
+//    printf("\n");
+
+    FILE *fptr;
+    fptr = fopen("words.txt", "r");
+
+    if (fptr == NULL){
+        printf("File cannot open!");
+    }
+
+    char c = fgetc(fptr);
+    while (!feof(fptr)){
+
+        if(name[0] == c){
+            printf("%c", c);
+            c = fgetc(fptr);
+            for (int j = 1; j < 7; j++) {
+                if (name[j] == c){
+                    printf("%c", c);
+                    c = fgetc(fptr);
+                }
+            }
+        }
+        c = fgetc(fptr);
+    }
+    fclose(fptr);
     return 0;
 }
 
-void printWords(){
-    FILE *fptr;
-    fptr = fopen("words.txt", "r");
-
-    if(fptr == NULL) {
-        printf("Your file can not open!");
-    } else{
-        char c = fgetc(fptr);
-        while (!feof(fptr)){
-            printf("%c", c);
-            c = fgetc(fptr);
-        }
-    }
-    fclose(fptr);
-}
-
-void findWords(){
-    int startPoint = 103;
-    int stopPoint = 120;
-    int j = 106;
-    int k = 107;
-    int l = 108;
-    int m = 109;
-    int o = 111;
-    int p = 112;
-    int q = 113;
-    int r = 114;
-    int s = 115;
-    int v = 118;
-
-    int delChar[10]{106, 107, 108, 109, 111, 112, 113, 114, 115,118};
-
-    FILE *fptr;
-    fptr = fopen("words.txt", "r");
-
-    char c = fgetc(fptr);
-    while (!feof(fptr)) {
-
-
-            if (c > startPoint && c < stopPoint) {
-
-                printf("%c", c);
-            }
-
-
-        c = fgetc(fptr);
-    }
-
-    fclose(fptr);
-}
-
-void findWord2(){
-    int index = 0;
-    FILE *fptr;
-    fptr = fopen("words.txt", "r");
-    char c = fgetc(fptr);
-    for (int i = 0; i < 10000; i++) {
-        if(toFind1[index] == c ){
-            printf("%c", c);
-            index = index + 1;
-        }
-        c = fgetc(fptr);
-    }
-    fclose(fptr);
-
-}
 
 //ရသလောက်လေးရေးထားတာပါဆရာ
 //လည်နေလို့ပါ
